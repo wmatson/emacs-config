41501e14449f9071397b2cc3b44602fa7f5157ee
Wesley Matson <wmatson@arbfund.com>
2018-04-25 15:06:17 -0600

    Adding the pos-tip config to flycheck, also happened to update all installed packages through the list-packages menu

--- lisp/cfg-flycheck.el
+++ lisp/cfg-flycheck.el
@@ -2,8 +2,18 @@
 (require-package 'let-alist)
 (require-package 'flycheck-clojure)
 (require-package 'use-package)
+(require-package 'flycheck-pos-tip)
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+(eval-after-load 'flycheck
+  '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
+
+
+(setq flycheck-pos-tip-timeout -1)
+
+(eval-after-load 'flycheck
+  (flycheck-pos-tip-mode))
 
 (use-package flycheck-clojure
   :commands (flycheck-clojure-setup)
